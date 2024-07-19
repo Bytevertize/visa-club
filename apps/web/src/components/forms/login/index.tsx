ef22a9facfbf5c0db383856effd910b5f926814a
BAZhelev <boyanz35@gmail.com>
2024-07-19 18:39:42 +0300

    feat: update form ui

--- apps/web/src/components/forms/login/index.tsx
+++ apps/web/src/components/forms/login/index.tsx
@@ -1,46 +1,60 @@
 'use client'
 
-import { useCurrentLocale } from 'next-i18n-router/client'
-import Link from 'next/link'
-import { i18nConfig } from '@i18n/config'
+// import Link from 'next/link'
+import { UserCircleIcon, LockClosedIcon } from '@heroicons/react/16/solid'
+import type { Locale } from '@i18n/types'
 import translation from './translations.json'
 
-export function LoginForm() {
-    const locale = useCurrentLocale(i18nConfig)
-    const emailText = locale
-        ? translation[locale as keyof typeof translation].email
-        : 'Email'
+type Props = {
+    locale: Locale
+}
 
-    const passwordText = locale
-        ? translation[locale as keyof typeof translation].password
-        : 'Password'
+export function LoginForm({ locale }: Props) {
+    const emailText = translation[locale as keyof typeof translation].email
 
-    const forgotPassword = locale
-        ? translation[locale as keyof typeof translation]['forgot-password']
-        : 'Back to Login'
+    const passwordText =
+        translation[locale as keyof typeof translation].password
 
-    const buttonText = locale
-        ? translation[locale as keyof typeof translation].submit
-        : 'Submit'
+    // const forgotPassword =
+    //     translation[locale as keyof typeof translation]['forgot-password']
+
+    const buttonText = translation[locale as keyof typeof translation].submit
 
     return (
-        <form action="/api/login-company" method="POST">
-            <input
-                id="emailLoginField"
-                name="emailLoginField"
-                placeholder={emailText}
-                required
-                type="email"
-            />
-            <input
-                id="passwordLoginField"
-                name="passwordLoginField"
-                placeholder={passwordText}
-                required
-                type="password"
-            />
-            <Link href={`/${locale}/forgot-password`}>{forgotPassword}</Link>
-            <button type="submit">{buttonText}</button>
+        <form
+            action="/api/login-company"
+            className="flex-1 flex flex-col overflow-auto max-h-screen w-full gap-2 items-center px-5 py-5 mx-auto max-w-[400px]"
+            method="POST"
+        >
+            <label className="flex items-center gap-2 input input-bordered input-ghost bg-transparent min-w-[200px] w-full">
+                {emailText}
+                <UserCircleIcon className="h-4 w-4 opacity-70 z-[-1]" />
+                <input
+                    className=""
+                    id="emailLoginField"
+                    name="emailLoginField"
+                    required
+                    type="email"
+                />
+            </label>
+
+            <label className="flex items-center gap-2 input input-bordered input-ghost bg-transparent min-w-[200px] w-full">
+                {passwordText}
+                <LockClosedIcon className="h-4 w-4 opacity-70 z-[-1]" />
+                <input
+                    id="passwordLoginField"
+                    name="passwordLoginField"
+                    required
+                    type="password"
+                />
+            </label>
+            {/* <Link href={`/${locale}/forgot-password`}>{forgotPassword}</Link> */}
+            <button
+                className="btn btn-primary min-w-[200px] w-full"
+                type="submit"
+            >
+                {buttonText}
+            </button>
         </form>
     )
 }
