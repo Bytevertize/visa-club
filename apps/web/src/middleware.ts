7e9a94cedd1f704598b1a68b71561ff48493a592
BAZhelev <boyanz35@gmail.com>
2024-07-22 18:13:43 +0300

    feat: update midlewear to protect /companies/* routes

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,8 +1,37 @@
 import { i18nRouter } from 'next-i18n-router'
-import type { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 import { i18nConfig } from '@i18n'
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
+    if (request.nextUrl.pathname.includes('companies')) {
+        const cmsCookieValue = request.cookies.get('payload-token')?.value
+        const cmsCookieName = request.cookies.get('payload-token')?.name
+
+        if (!cmsCookieValue) {
+            return NextResponse.redirect(new URL('/', request.url), {
+                status: 302,
+            })
+        }
+
+        const cmsResponse = await fetch(
+            `${process.env.NEXT_PUBLIC_PAYLOAD_ENDPOINT}/api/company/me`,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Cookie: `${cmsCookieName}=${cmsCookieValue}`,
+                },
+            },
+        )
+
+        const cmsData = await cmsResponse.json()
+
+        if (!cmsData.token) {
+            return NextResponse.redirect(new URL('/', request.url), {
+                status: 302,
+            })
+        }
+    }
+
     return i18nRouter(request, i18nConfig)
 }
 
